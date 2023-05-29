3f469a8f03806efd427a780fc92e7ceecb69ec32
Philipe <philipe.tavares@skyone.solutions>
2023-05-28 23:02:22 -0300

    updated form fields and lib ref

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ driver = webdriver.Chrome()
 
 driver.get("https://www.anitta.com.br/contato/")
 
-elem = driver.find_element_by_name("name")
+elem = driver.find_element("name", "us_form_1_text_1")
 elem.clear()
 elem.send_keys("Neymar")
 
-elem = driver.find_element_by_name("email")
+elem = driver.find_element("name", "us_form_1_email_1")
 elem.clear()
 elem.send_keys("neymarjr@gmail.com")
 
-elem = driver.find_element_by_name("message")
+elem = driver.find_element("name", "us_form_1_textarea_1")
 elem.clear()
 elem.send_keys("Oi razão da minha libido rsrs")
 
-elem = driver.find_element_by_name("captcha")
+elem = driver.find_element("name", "us_form_1_captcha_1")
 elem.clear()
-elem.send_keys("17")
+elem.send_keys("6")
+
+sleep(10)
 
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
